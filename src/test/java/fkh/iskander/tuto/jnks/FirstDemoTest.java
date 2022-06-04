373d845a7ab2c29a3cb11b24bae00b9339e295f3
Iskander FEKIH <sh.fekih@gmail.com>
2022-06-04 01:23:06 +0100

    adding fail

---
+++ src/test/java/fkh/iskander/tuto/jnks/FirstDemoTest.java
@@ -0,0 +1,14 @@
+package fkh.iskander.tuto.jnks;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirstDemoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
